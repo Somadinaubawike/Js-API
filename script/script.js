1768fc12e156e38a3d3ad9489df6fbc13e99f917
Somadinaubawike <ubawikesomadina@gmail.com>
2021-11-02 18:34:27 +0000

    third commit

--- script/script.js
+++ script/script.js
@@ -1,4 +1,8 @@
+// let postTitle = document.getElementById('post-title')
+// let postBody = document.getElementById('post-body')
+// let postForm = document.querySelector('#post-form')
 
+// postForm.addEventListener('submit', createPost)
 
 function getPost () {
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -10,17 +14,19 @@ function getPost () {
         let html = "";
         data.forEach(e => {
             html += `
-                <div class="col-4 mb-3">
-                    <div class="post-card bg-primary h-100">
-                        <div class="card-body text-white">
-                            <div class ="d-flex justify-content-end">
-                                <h6>${e.id}</h6>
+                    <div class="col-4 mb-3">
+                        <a href="page.html">
+                            <div class="post-card bg-primary h-100">
+                                <div class="card-body text-white">
+                                    <div class ="d-flex justify-content-end">
+                                        <h6>${e.id}</h6>
+                                    </div>
+                                    <h5 class="post-title mb-4">${e.title}</h5>
+                                    <p class="post-body">${e.body}</p>
+                                </div>
                             </div>
-                            <h5 class="post-title mb-4">${e.title}</h5>
-                            <p class="post-body">${e.body}</p>
-                        </div>
+                        </a> 
                     </div>
-                </div>
             `
             postLayout.innerHTML = html
         });
@@ -29,6 +35,49 @@ function getPost () {
 
 getPost();
 
-function createPost() {
-    
+
+function getSinglePost () {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data)
+
+        let postSingle = document.getElementById('post-single')
+        let html = "";
+        data.forEach(e => {
+            html += `
+                <div class="card-body">
+                    <div class ="d-flex justify-content-end">
+                        <h6>${e.id}</h6>
+                    </div>
+                    <h5 class="post-title mb-4">${e.title}</h5>
+                    <p class="post-body">${e.bobdy}</p>
+                </div>
+            `
+            postSingle.innerHTML = html
+        });
+    })
 }
+
+getSinglePost();
+
+
+// function createPost(e) {
+//     e.preventDefault();
+//     let pTitle = postTitle.Value;
+//     let pBody = postBody.Value;
+
+//     fetch('https://jsonplaceholder.typicode.com/posts', {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             title: 'pTitle',
+//             body: 'pBody',
+//             userId: '3'
+//         }),
+//         headers: {
+//             'content-type': 'application/json; charset=UTF-8'
+//         },
+//     })
+//     .then((response) => response.json())
+//     .then ()
+// }
